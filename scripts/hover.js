7c2dbad921a9c4e7e3a0643c6db7d94288eaa18f
Carl Schmidt <casaschmidt@live.com>
2012-06-04 12:01:17 -0700

    First Commit
    Adding the Furry 5K site files to github

---
+++ scripts/hover.js
@@ -0,0 +1,7 @@
+﻿     $(document).ready(function(){	   
+		$('img.navElement').hover(function() {
+		$(this).attr("src","img/" + this.name + "_over.gif");
+			}, function() {
+		$(this).attr("src","img/" + this.name + "_off.gif");
+		});
+     });
